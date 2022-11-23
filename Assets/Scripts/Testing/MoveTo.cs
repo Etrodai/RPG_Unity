715f87d59831fd6645642dbe9b645d5d1c9dfb53
Dragenix1 <erichaas21@gmail.com>
2022-11-23 19:21:49 +0100

    Commented MoveTo testing script

--- Assets/Scripts/Testing/MoveTo.cs
+++ Assets/Scripts/Testing/MoveTo.cs
@@ -3,27 +3,27 @@ using UnityEngine.AI;
 
 namespace Testing
 {
-    public class MoveTo : MonoBehaviour //Made by Ben
+    public class MoveTo : MonoBehaviour //Made by Ben (currently unused)
     {
-        [SerializeField] private NavMeshAgent agent;
-        [SerializeField] private GameObject target;
+        //[SerializeField] private NavMeshAgent agent;
+        //[SerializeField] private GameObject target;
 
-        private void Awake()
-        {
-            agent = GetComponent<NavMeshAgent>();
-        }
+        //private void Awake()
+        //{
+        //    agent = GetComponent<NavMeshAgent>();
+        //}
 
-        private void Update()
-        {
-            Vector3 targetPos = target.transform.position;
+        //private void Update()
+        //{
+        //    Vector3 targetPos = target.transform.position;
 
-            agent.SetDestination(targetPos);
+        //    agent.SetDestination(targetPos);
 
 
-            if (agent.remainingDistance <= 15f)
-            {
-                agent.ResetPath();
-            }
-        }
+        //    if (agent.remainingDistance <= 15f)
+        //    {
+        //        agent.ResetPath();
+        //    }
+        //}
     }
 }
